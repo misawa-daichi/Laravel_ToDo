6ccce69a751b0e9e5dcf0c7696ecb61be0ad3a17
misawa_daichi <moyasipp26@gmail.com>
2021-12-30 15:09:29 +0900

    taskコントローラー作成

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TaskController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/folders/{id}/tasks', 'App\Http\Controllers\TaskController@index')->name ('tasks.index');
